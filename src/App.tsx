edca0202c011585d1cb419a4b45ace8e53de2fdb
leahwilliams1315 <leah1315@gmail.com>
2020-11-27 09:03:52 -0500

    init commit, server up and running

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    fetch('/nodes')
+      .then(data => data.json())
+      .then(result => console.log(result));
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
